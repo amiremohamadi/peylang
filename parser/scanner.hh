a19ebb6f41c8b9c1b7e9a82c6164a8eab7e7fcf6
amiremohamadi <amiremohamadi@yahoo.com>
2020-07-12 19:10:16 +0430

    update scanner to work correctly with parser

--- parser/scanner.hh
+++ parser/scanner.hh
@@ -5,11 +5,21 @@
 #include <FlexLexer.h>
 #endif
 
+#include "location.hh"
+#include "parser.tab.hh"
+
 namespace pey {
 class Scanner : public yyFlexLexer {
+private:
+  pey::Parser::semantic_type *yylval = nullptr;
+
 public:
   Scanner(std::istream *input);
-  virtual int yylex();
+
+  // get rid of overriding virtual function warning
+  using FlexLexer::yylex;
+  virtual int yylex(pey::Parser::semantic_type *const lval,
+                    pey::Parser::location_type *location);
 };
 } // namespace pey
 
