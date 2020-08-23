4dccaff9dcfda3e3c113898fdd01d2c62326fe4b
amiremohamadi <amiremohamadi@yahoo.com>
2020-08-23 16:57:58 +0430

    remove legacy files

--- parser/scanner.cc
+++
@@ -1,8 +0,0 @@
-#include "scanner.hh"
-
-pey::Scanner::Scanner(std::istream *input) : yyFlexLexer(input) {
-  // Scanner class is not handling memory management and
-  // you should be very careful about dangling pointer and memory leaking
-
-  // unimplemented
-}
