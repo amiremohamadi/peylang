2ed047a3e390bcf45b85219e00fab072c52b3d46
amiremohamadi <amiremohamadi@yahoo.com>
2020-08-30 13:56:49 +0430

    add welcome message

--- peyman.cc
+++ peyman.cc
@@ -1,12 +1,38 @@
 #include <cstdio>
+#include <iostream>
 #include <parser.tab.h>
+
+#define VERSION "0.0.1"
 
 extern int yyparse();
 extern FILE *yyin;
 
 int main(int argc, char **argv) {
 
-  yyin = fopen(argv[1], "r+");
+  if (argc > 1) {
+    yyin = fopen(argv[1], "r+");
+  } else {
+    std::cout << R"(
+  _  _   /'/    )\,/,/(_   \ \
+   `//-.|  (  ,\\)\//\)\/_  ) |
+   //___\   `\\\/\\/\/\\///'  /
+,-"~`-._ `"--'_   `"""`  _ \`'"~-,_
+\       `-.  '_`.      .'_` \ ,-"~`/
+ `.__.-'`/   (-\        /-) |-.__,'
+   ||   |     \O)  /^\ (O/  |
+   `\\  |         /   `\    /
+     \\  \       /      `\ /
+      `\\ `-.  /' .---.--.\
+        `\\/`~(, '()      ('
+         /(O) \\   _,.-.,_)
+        //  \\ `\'`      /
+  pey  / |  ||   `""""~"`
+     /'  |__||
+           `o 
+		)"
+              << "\n";
+    std::cout << "Peyman programming language " << VERSION << "\n\n";
+  }
   yyparse();
 
   return 0;
