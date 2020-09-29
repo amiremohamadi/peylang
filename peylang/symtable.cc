12e044eff01998cbe857e1059f32575b63443d23
amiremohamadi <amiremohamadi@yahoo.com>
2020-09-29 12:40:05 +0330

    add "count" method to check if variable is present

--- peylang/symtable.cc
+++ peylang/symtable.cc
@@ -1,8 +1,12 @@
 #include <symtable.hh>
 
 namespace pey {
+
 Object &Symtable::operator[](std::string ident) {
   // TODO: throw exception in case of not finding the ident
   return this->_lookup[ident];
 }
+
+int Symtable::count(std::string ident) { return this->_lookup.count(ident); }
+
 } // namespace pey
